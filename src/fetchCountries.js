6fdf09a4bb736a42414ceb53079d9cb8ed6ab172
OlesiaKubska <kublesia0908@gmail.com>
2023-05-13 20:59:27 +0200

    update index.js and add styles.css

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,19 +1,20 @@
 // fetchCountries.js
+
 const URL = `https://restcountries.com/v3.1/name/`;
+//створюємо URL для запиту до API країн за допомогою вказаного імені, 
+//а також додаємо параметри пошуку, які включають потрібні властивості країни.
 
 const searchParams = new URLSearchParams({
     fields: 'name,capital,population,flags,languages,',
 });
 
 
-export function fetchCountries(name) {    
-
+export const fetchCountries = (name) => {
     return fetch(`${URL}${name}?${searchParams.toString()}`)
         .then(response => {
-            if (response.status === 404) {
+            if (!response.ok) {
                 throw new Error(response.status);
             }
             return response.json();
         });
-    
 };
--- src/index.js
+++ src/index.js
@@ -4,36 +4,33 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries.js';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 300; //Встановлює затримку DEBOUNCE_DELAY в 300 мс.
 
+//Отримує посилання на елементи DOM, такі як список країн (countriesList)
 const countriesList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-const searchBox = document.querySelector('#search-box');
-const body = document.querySelector('body');
-
-countriesList.style.visibility = 'hidden';
-countryInfo.style.visibility = 'hidden';
+const countryInfo = document.querySelector('.country-info'); //інформація про країну
+const searchBox = document.querySelector('#search-box'); //поле пошуку (searchBox)
 
 // Додаємо обробник події 'input' на поле пошуку
 searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch() {
-    const searchValue = searchBox.value.trim();
+    const searchValue = searchBox.value.trim(); //Отримується значення пошуку з поля введення та проводиться санітизація рядка методом trim()
 
     // Якщо поле пошуку порожнє, очищаємо розмітку списку країн та інформації про країну
     if (searchValue === '') {
         clearMarkup();
         return;
     }
-
+//Виконується HTTP-запит до функції fetchCountries з передачею значення пошуку та властивостей країни для отримання даних. 
     fetchCountries(searchValue, 'name.official,capital,population,flags.svg,languages')
     
         .then(renderCountryList)
         .catch(error => Notify.failure(error));
 }
-
+//При отриманні результату викликається функція renderCountryList для відображення списку країн або повідомлення про помилку.
 function renderCountryList(countries) {
-    clearMarkup();
+    clearMarkup(); //Очищує розмітку списку країн.
 
     if (countries.length === 0) {
         // Якщо не знайдено країну, виводимо повідомлення про помилку
@@ -47,42 +44,46 @@ function renderCountryList(countries) {
     }
 }
 
+// функція displayCountryList(countries) отримує список країн
 function displayCountryList(countries) {
+    //Використовуючи метод forEach, перебирає кожну країну у списку
     countries.forEach(country => {
-        const { flags: { svg }, name: { official } } = country;
+        const { flags: { svg }, name: { official } } = country; //Деструктурує властивості flags.svg та name.official з кожної країни.
 
-        const listItem = document.createElement('li');
+        const listItem = document.createElement('li');//Створює новий елемент <li> для кожної країни.
         listItem.innerHTML = `<img src="${svg}" alt="${official} flag" /> ${official}`;
 
+//Додає обробник події click до кожного елементу <li>, що викликає функцію displayCountryInfo з об'єктом країни як аргумент.
         listItem.addEventListener('click', () => {
             displayCountryInfo(country);
         });
 
-        countryList.appendChild(listItem);
+        countriesList.appendChild(listItem);//Додає елемент <li> до списку країн (countriesList).
     });
 
     clearCountryInfo();
 }
 
-
+//функція displayCountryInfo(country) отримує об'єкт країни
 function displayCountryInfo(country) {
-    clearCountryInfo();
-    
+    clearCountryInfo(); //Викликає функцію clearCountryInfo для очищення попередньої інформації про країну.
+    //Деструктурує: 
     const { flags: { svg }, name: { official }, capital, population, languages } = country;
-
+//Створює розмітку для відображення інформації про країну, використовуючи отримані властивості.
+    //Присвоює отриману розмітку змінній countryInfoMarkup.
     const countryInfoMarkup = `
         <div class="country-info__flag">
-            <img src="${svg}" alt="${official} flag" />
+            <img src="${svg}" alt="${official} flag" width="40" height="20" />
         </div>
         <div class="country-info__details">
             <h2>${official}</h2>
             <p><strong>Capital:</strong> ${capital}</p>
             <p><strong>Population:</strong> ${population.toLocaleString()}</p>
             <p><strong>Languages:</strong> ${languages.map(lang => lang.name).join(', ')}</p>
-        </div>
-`;
+        </div>`;
 
-countryInfo.innerHTML = countryInfoMarkup;
+    countryInfo.innerHTML = countryInfoMarkup; //Заміщує вміст елементу countryInfo отриманою розміткою, щоб відобразити інформацію про країну.
+    return countryInfoMarkup; //Повертає розмітку countryInfoMarkup для можливого використання в інших частинах коду.
 }  
 
 function clearMarkup() {
@@ -91,9 +92,9 @@ function clearMarkup() {
 }
 
 function clearCountryList() {
-    countryList.innerHTML = '';
+    countriesList.innerHTML = ''; //Функція clearCountryList присвоює порожній рядок властивості innerHTML елемента countriesList, що призводить до очищення списку країн.
 }
 
 function clearCountryInfo() {
-    countryInfo.innerHTML = '';
+    countryInfo.innerHTML = ''; //Функція clearCountryInfo присвоює порожній рядок властивості innerHTML елемента countryInfo, що призводить до очищення відображеної інформації про країну.
 }
